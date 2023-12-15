7ceedf27c84aff8aef9d3a434dedfd16383ab644
RanaAwais <mixterraj456@gmail.com>
2023-12-15 21:26:48 +0500

    adding the product and making the product component

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { ProductsContext } from "../../contexts/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
+import "./shop.style.scss";
+export const Shop = () => {
+  const { products } = useContext(ProductsContext);
+  console.log("Products are", products);
+  return (
+    <div className="product-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
